f2604c64f14e20c1497d17bcb11a6657ace4af11
marcin <marcin@marcin-K50IJ>
2015-03-05 19:10:25 +0100

    role

--- protected/config/main.php
+++ protected/config/main.php
@@ -100,6 +100,7 @@ return array(
 		'adminEmail'=>'maciej.krawczyk@mjkr.pl',
 		'meta_description' => 'tu wpisz opis strony',
 		'meta_keywords' =>'slowa, kluczowe, z przecinakami',
-		'roles'=>array('administrator'=>'administrator','menager'=>'menedżer zespołu','telemarketer'=> 'telemarketer','redaktor'=>'redaktor')
+			
+		'roles'=>array('administrator'=>'administrator','koordynator'=>'koordynator', 'redaktor'=>'redaktor', 'telemarketer'=> 'telemarketer')
 	),
 );
